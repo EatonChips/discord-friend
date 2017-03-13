a1765d58e3b754c07b4c32aafd321d1e02902ae8
Adam Eaton <tacticalpterodactyl@gmail.com>
2017-03-13 13:28:17 -0500

    config commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    CBOT_KEY: 'CC12dEGBVZykVVJ1LuUh8BK0jkQ',
-    DBOT_KEY: 'MjgxODcwNzQ1NjExNjY1NDA4.C4eQLQ.dJhPeFaCbr-plBBEDVQwX06qM_M'
+    CBOT_KEY: '<CLEVERBOT_API_KEY>',
+    DBOT_KEY: '<DISCORD_API_TOKEN>'
 }
